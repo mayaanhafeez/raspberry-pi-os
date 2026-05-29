73ba06ec50c37aeb71d8cd472e80e940d85cc25b
Ayaan Hafeez <m.ayaan.hafeez@gmail.com>
2026-05-30 02:02:05 +0500

    lesson 3

--- build/kernel_c.d
+++ build/kernel_c.d
@@ -1,2 +1,2 @@
 build/kernel_c.o: src/kernel.c include/mini_uart.h include/utils.h \
- include/printf.h
+ include/printf.h include/timer.h include/irq.h
--- src/kernel.c
+++ src/kernel.c
@@ -1,10 +1,16 @@
 #include "mini_uart.h" 
 #include "utils.h"
 #include "printf.h"
+#include "timer.h"
+#include "irq.h"
 
 void kernel_main(void){
   uart_init();
   init_printf(0, putc);
+  irq_vector_init();
+  timer_init();
+  enable_interrupt_controller();
+  enable_irq();
   int el= get_el();
   printf("Exception Level: %d \r\n",el);
 
